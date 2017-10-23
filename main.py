a442323361120cad098565b3dd18f827476f04a3
Luteek <develop.luttek@gmail.com>
2017-10-23 18:15:31 +0300

    3

--- main.py
+++ main.py
@@ -13,16 +13,12 @@ def fping_(ip, parameters):
 def data_(cmd):
     while True:
         if cmd.returncode:
-            output = cmd.communicate()[0]
+            output = cmd.stdout.readline()
             output = str(output)
             print("NUMBER: ", output)
         yield from asyncio.sleep(4)
 
 print('hello')
-try:
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(fping_('8.8.8.8', ' -Q 2 -p 50 -l'))
-except:
-    print('some')
-finally:
-    print('End')
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(fping_('8.8.8.8', ' -Q 2 -p 50 -l'))
